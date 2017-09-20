3b95c7cc848266733c9ff8d5a9c8525b5fc66b9c
Shyam Kansagra <shyamkansagra@gmail.com>
2017-09-20 19:39:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-  const getParamsFromInputs = () => {
-	const noOfSessions = document.getElementById('sessions').value;
-	const minutesPerPomodoro = document.getElementById('minutes-per-pomodoro').value;
-	const minutesPerBreak = document.getElementById('minutes-per-break').value;
-	return { noOfSessions, minutesPerPomodoro, minutesPerBreak };
-};
+const PATTERN_lENGTH = 20;
 
 const addEventListeners = () => {
-	document.getElementById('Regular').addEventListener('click', createPattern);
-	document.getElementById('Strict').addEventListener('click', createPattern;
-	document.getElementById('B').addEventListener('click', appendClicks;
-	document.getElementById('R').addEventListener('click', appendClicks);
-	document.getElementById('G').addEventListener('click', appendClicks;
-	document.getElementById('Y').addEventListener('click', appendClicks);
+	document.getElementById('Regular').addEventListener('click', createPattern(this));
+	document.getElementById('Strict').addEventListener('click', createPattern(this));
+	document.getElementById('B').addEventListener('click', appendClicks(this));
+	document.getElementById('R').addEventListener('click', appendClicks(this));
+	document.getElementById('G').addEventListener('click', appendClicks(this));
+	document.getElementById('Y').addEventListener('click', appendClicks(this));
 };
+
+let createPattern = (mode) => {
+	document.getElementById("gameMode").value = mode.id;
+  	document.getElementById("stepCount").innerHTML = "!!";
+  	document.getElementById("dashText").innerHTML = "New Game!";
+  	
+  	let text = "";
+  	const possibleChars = "RGBY";
+
+  	let patternText = generateRandomPattern(text, PATTERN_lENGTH, possibleChars);
+  	
+  	document.getElementById("pattern").value = text;
+  	showSteps();
+};
+
 
 addEventListeners();
