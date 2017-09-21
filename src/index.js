dc66ce330422c4239d6c59abb05b84f944e0d358
Shyam Kansagra <shyamkansagra@gmail.com>
2017-09-21 12:32:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,66 @@
 const PATTERN_lENGTH = 20;
 
-const addEventListeners = () => {
-	document.getElementById('Regular').addEventListener('click', createPattern(this));
-	document.getElementById('Strict').addEventListener('click', createPattern(this));
-	document.getElementById('B').addEventListener('click', appendClicks(this));
-	document.getElementById('R').addEventListener('click', appendClicks(this));
-	document.getElementById('G').addEventListener('click', appendClicks(this));
-	document.getElementById('Y').addEventListener('click', appendClicks(this));
-};
-
 let createPattern = (mode) => {
-	document.getElementById("gameMode").value = mode.id;
+	  document.getElementById("gameMode").value = mode.id;
   	document.getElementById("stepCount").innerHTML = "!!";
   	document.getElementById("dashText").innerHTML = "New Game!";
   	
-  	let text = "";
+  	let textPattern = "";
   	const possibleChars = "RGBY";
 
-  	let patternText = generateRandomPattern(text, PATTERN_lENGTH, possibleChars);
+  	let patternData = generateRandomPattern(textPattern,PATTERN_lENGTH,possibleChars);
   	
-  	document.getElementById("pattern").value = text;
+  	document.getElementById("pattern").value = patternData;
   	showSteps();
 };
 
+let showSteps = (repeatation = "") => {
+  let pattern = document.getElementById("pattern").value; 
+  let steps = document.getElementById("stepCount").textContent;
 
-addEventListeners();
+  let newStepValue = stepsUpdation(steps,repeatation);
+
+  document.getElementById("stepCount").innerHTML = newStepValue;
+  document.getElementById("dashText").innerHTML = "Remember the pattern shown!";
+  document.getElementById("lockStatus").value = "unlocked";
+  document.getElementById("userInput").value = "";
+  alert("Pattern: "+pattern.slice(0,newStepValue));
+};
+
+let appendClicks = (divisionId) => {
+  let lockStatus = document.getElementById("lockStatus").value;
+  let oldUserInputVal = document.getElementById("userInput").value;
+
+  let newUserInputVal = latestUserInput(oldUserInputVal,divisionId.id);
+  document.getElementById("userInput").value = newUserInputVal;
+
+  let noOfSteps = document.getElementById("stepCount").value;
+  let pattern = document.getElementById("pattern").value;
+  let rightPattern = pattern.slice(0,parseInt(newUserInputVal.length));
+
+  if(newUserInputVal != rightPattern) {
+      alert("Wrong slice clicked!!");
+      if(document.getElementById("gameMode").value == "Regular") {
+        showSteps("repeat");
+        return "Repeat Step!";
+      }
+      else {
+        document.getElementById("stepCount").innerHTML = "!!";
+        createPattern({"id":"Strict"});
+        return "New Game!";
+      }
+    }
+
+    noOfSteps = document.getElementById("stepCount").textContent;
+    if(newUserInputVal.length == parseInt(noOfSteps)) {
+      if(newUserInputVal.length == 20) {
+        alert("Congratulations! You Won!!");
+        document.getElementById("dashText").innerHTML = "!!Winner!!";
+        document.getElementById("lockStatus").value = "locked";
+        document.getElementById("stepCount").innerHTML = "!!";
+      }
+      else {
+        showSteps();
+      }
+    }
+};
