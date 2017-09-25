6155fa6eb576594f876c60bba5ea43f01097fdf5
Shyam Kansagra <shyamkansagra@gmail.com>
2017-09-25 14:34:54 +0530

    Removed updation of hidden fields

--- src/index.js
+++ src/index.js
@@ -12,33 +12,16 @@ const addEventListeners = () => {
 let simonGameController = () => {
   return new SimonGame(
     (gameState) => {
-      if(gameState.gameMode === "strict") {
-        document.getElementById('gameMode').value = "strict";
-        document.getElementById('lockStatus').value = gameState.lockStatus;
-      }
-      if(gameState.gameMode === "regular") {
-        document.getElementById('gameMode').value = "regular";
-        document.getElementById('lockStatus').value = gameState.lockStatus;
-      }
       if(gameState.newGame && gameState.generatedRandomPattern !== "") {
         document.getElementById("stepCount").innerHTML = "!!";
         document.getElementById("dashText").innerHTML = "New Game!";
-        document.getElementById("pattern").value = gameState.generatedRandomPattern;
-        document.getElementById("lockStatus").value = gameState.lockStatus;
       }
       if(gameState.showPattern) {
         document.getElementById("stepCount").innerHTML = gameState.steps;
         document.getElementById("dashText").innerHTML = "Remember the pattern shown!";
-        document.getElementById("lockStatus").value = gameState.lockStatus;
-        document.getElementById("userInput").value = "";
         alert("Pattern: "+gameState.currentPattern);
       }
-      if(gameState.userInput) {
-        document.getElementById("userInput").value = gameState.userInput;
-        document.getElementById("lockStatus").value = gameState.lockStatus;
-      }
       if(gameState.wrongClickByUser) {
-        document.getElementById("lockStatus").value = gameState.lockStatus;
         alert("Wrong slice clicked!!");
         if (gameState.gameMode === "strict") {
           document.getElementById('stepCount').innerHTML = "!!";
@@ -47,7 +30,6 @@ let simonGameController = () => {
       if(gameState.playerWin) {
         alert("Congratulations! You Won!!");
         document.getElementById("dashText").innerHTML = "!!Winner!!";
-        document.getElementById("lockStatus").value = gameState.lockStatus;
         document.getElementById("stepCount").innerHTML = "!!";
       }
     }
@@ -95,7 +77,6 @@ const finishComputerMove = () => {
   simon.validateUserInput();
   let gameState = simon.getGameState();
   if(gameState.wrongClickByUser) {
-    //alert("inside wrong clicked");
     if(gameState.gameMode === 'regular') {
       simon.showPattern('repeat');
     }
@@ -108,7 +89,6 @@ const finishComputerMove = () => {
   else { //User Input is correct
     gameState = simon.getGameState();
     if(gameState.userInput.length === gameState.steps) {
-      //alert(gameState.steps);
       let gameWon = gameState.playerWin;
       if(!gameWon) {
         simon.showPattern();
