1782d0f41e3cc8e1a40be7f7470d1b985cd5e774
Shyam Kansagra <shyamkansagra@gmail.com>
2017-09-27 23:20:41 +0530

    Even more refactoring

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
-let simon = null;
-
 const addEventListeners = () => {
-  const gameModes = ['regular' , 'strict'];
-  gameModes.forEach((gameMode) => 
-    document.getElementById(gameMode).addEventListener('click',function() {
-    simon.startGame(gameMode);
-  }));
+  const gameModes = ['regular', 'strict'];
+  gameModes.forEach(
+    (gameMode) => document
+      .getElementById(gameMode)
+      .addEventListener('click', () => simon.startGame(gameMode))
+  );
 
   const sliceColors = ['B', 'R', 'G', 'Y'];
-  sliceColors.forEach((sliceColor) => 
-    document.getElementById(sliceColor).addEventListener('click',function() {
-    simon.processUserInput(sliceColor);
-  }));
-};
-
-let simonGameController = () => {
-  return new SimonGame(
-    (gameState) => {
-      if(gameState.showPattern) {
-        document.getElementById("stepCount").innerHTML = gameState.steps;
-        document.getElementById("dashText").innerHTML = "Remember the pattern shown!";
-        alert("Pattern: "+gameState.currentPattern);
-      }
-      if(gameState.wrongClickByUser) {
-        alert("Wrong slice clicked!!");
-        simon.resetGame();
-      }
-      if(gameState.playerWin) {
-        alert("Congratulations! You Won!!");
-        document.getElementById("dashText").innerHTML = "!!Winner!!";
-        document.getElementById("stepCount").innerHTML = "!!";
-      }
-    }
+  sliceColors.forEach(
+    (sliceColor) => document
+      .getElementById(sliceColor)
+      .addEventListener('click', () => simon.processUserInput(sliceColor))
   );
 };
 
-simon = simonGameController();
+const renderFn = (gameState) => {
+  if (gameState.showPattern) {
+    document.getElementById("stepCount").innerHTML = gameState.steps;
+    document.getElementById("dashText").innerHTML = "Remember the pattern shown!";
+    alert("Pattern: " + gameState.currentPattern);
+  }
 
+  if (gameState.wrongClickByUser) {
+    alert("Wrong slice clicked!!");
+    simon.resetGame();
+  }
+
+  if (gameState.playerWin) {
+    alert("Congratulations! You Won!!");
+    document.getElementById("dashText").innerHTML = "!!Winner!!";
+    document.getElementById("stepCount").innerHTML = "!!";
+  }
+};
+
+const simon = new SimonGame(renderFn);
 addEventListeners();
